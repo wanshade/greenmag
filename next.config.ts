9f9ef4e6f213f0f73537ddaf85572459dc9c28ff
Wawan Darmawan <41707274+wanshade@users.noreply.github.com>
2025-11-02 21:19:42 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
